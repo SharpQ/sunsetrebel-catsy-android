d2d97472c2c56dbe1cccdab8a9dcac6dee45733c
MSI\msiuser <gsashnev@gmail.com>
2021-06-19 16:56:33 +0300

    New navigation bar created; Fragments perfomance imrpoved; Files and variables names fixed; Nav bar and event fonts fixed; Some small fixes.

---
+++ app/src/main/java/com/sunsetrebel/catsy/activities/HomeBackActivity.java
@@ -0,0 +1,16 @@
+package com.sunsetrebel.catsy.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sunsetrebel.catsy.R;
+
+public class cameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
