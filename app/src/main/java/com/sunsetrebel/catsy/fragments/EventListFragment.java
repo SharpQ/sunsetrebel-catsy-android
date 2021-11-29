a9697e7eea8414df07b4f679830b70076e5f9fcb
Aleksey Sheremet <a.sheremet@pnn.in.ua>
2021-11-29 23:43:05 +0200

    CAT: added progress bar on event list load

--- app/src/main/java/com/sunsetrebel/catsy/fragments/EventListFragment.java
+++ app/src/main/java/com/sunsetrebel/catsy/fragments/EventListFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,6 +19,7 @@ public class EventListFragment extends Fragment {
     private RecyclerView eventRecycler;
     private EventListViewModel eventListViewModel;
     private EventListAdapter eventListAdapter;
+    private ProgressBar progressBar;
 
     public EventListFragment() {
         // Required empty public constructor
@@ -30,6 +33,8 @@ public class EventListFragment extends Fragment {
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_event_list, container, false);
         eventRecycler = v.findViewById(R.id.recyclerViewEventList);
+        progressBar = v.findViewById(R.id.pb_event_list);
+        progressBar.setVisibility(View.VISIBLE);
         eventRecycler.setLayoutManager(new LinearLayoutManager(getContext()));
         //Init viewmodel
         eventListViewModel = new ViewModelProvider(this).get(EventListViewModel.class);
@@ -39,6 +44,7 @@ public class EventListFragment extends Fragment {
             eventListAdapter = new EventListAdapter(getContext(), eventList);
             eventRecycler.setAdapter(eventListAdapter);
             eventListAdapter.notifyDataSetChanged();
+            progressBar.setVisibility(View.INVISIBLE);
         });
         return v;
     }
