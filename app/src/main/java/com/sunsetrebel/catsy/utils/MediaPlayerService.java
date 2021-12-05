d738f31eca88801797afedb5351c8f1ed904cc9e
Aleksey Sheremet <a.sheremet@pnn.in.ua>
2021-12-06 01:12:59 +0200

    CAT: fixed bug with interrupted intro sound

--- app/src/main/java/com/sunsetrebel/catsy/utils/MediaPlayerService.java
+++ app/src/main/java/com/sunsetrebel/catsy/utils/MediaPlayerService.java
@@ -2,16 +2,20 @@ package com.sunsetrebel.catsy.utils;
 
 import android.content.Context;
 import android.media.MediaPlayer;
-
 import com.sunsetrebel.catsy.R;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MediaPlayerService {
+    private static Set<MediaPlayer> activePlayers = new HashSet<MediaPlayer>();
     public static void playIntro(Context context) {
         final MediaPlayer mp = MediaPlayer.create(context, R.raw.catsy_intro);
+        activePlayers.add(mp);
         mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
                 releaseMP(mp);
+                activePlayers.remove(mp);
             }
         });
         mp.start();
